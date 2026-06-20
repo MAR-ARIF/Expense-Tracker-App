d8c3942916b1b5ee206774089b398a87353c1fa2
Arif <arifmarahman19@gmail.com>
2026-06-20 12:26:49 +0100

    Header modified with new navButton

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
-import Button from "../Components/Button"
 import { useNavigate } from "react-router-dom";
+import NavButton from "../Components/navButton";
 
 function Header(){
     const authStatus = useSelector((state) => state.auth.status);
@@ -8,7 +8,7 @@ function Header(){
     const navItems = [
         {name: "Dashboard" , path: "/" , active : true },
         {name : "Transactions" , path: "/transactions" , active : authStatus },
-        {name : "Analytics" , path: "/analytics" , active : !authStatus},
+        {name : "Analytics" , path: "/analytics" , active : authStatus},
         {name : "Settings" , path: "/settings" , active : authStatus},
         {name : "SignUp" , path: "/signup" , active : !authStatus},
         {name: "Login" , path: "/login" , active : !authStatus}
@@ -19,9 +19,9 @@ function Header(){
                 {navItems.map((item) => 
                     item.active ? (
                         <li key={item.name}>
-                            <Button onClick={() => navigate(item.path)} >
+                            <NavButton onClick={() => navigate(item.path)} >
                                 {item.name}
-                            </Button>
+                            </NavButton>
                         </li>
                     ) : null
                 )}
