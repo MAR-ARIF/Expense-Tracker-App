75dd07348147c53a4fffb212aa27f952791466ee
Arif <arifmarahman19@gmail.com>
2026-06-20 12:25:16 +0100

    NavButton component added only to handle navBar buttons

---
+++ src/Components/NavButton.jsx
@@ -0,0 +1,13 @@
+function NavButton ({
+    type="button",
+    children,
+    className,
+    ...props
+}){
+    return (
+        <button className={`px-2 py-2 ${className}`} {...props} type={type}>
+            {children}
+        </button>
+    )
+}
+export default NavButton
