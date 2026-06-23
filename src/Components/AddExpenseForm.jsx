5a668981f98b397d87c30788b6b6a139ca41811d
Arif <arifmarahman19@gmail.com>
2026-06-23 16:46:14 +0100

    AddExpenseForm modified with modal features

--- src/Components/AddExpenseForm.jsx
+++ src/Components/AddExpenseForm.jsx
@@ -5,8 +5,9 @@ import Input from "./Input";
 import { useForm } from "react-hook-form";
 import Select from "./Select";
 import Button from "./Button";
+import TransactionList from "./TransactionList";
 
-function AddExpenseForm(){
+function AddExpenseForm({onSuccess}){
 
     const dispatch = useDispatch();
     const userData = useSelector((state) => state.auth.userData);
@@ -21,10 +22,15 @@ function AddExpenseForm(){
     })
     const submit = async(data) =>{
         try {
+            if(!userData ){
+                console.log("user not logged in");
+                return;
+            }
             const transaction = await databaseService.createExpense({...data ,amount : Number(data.amount), userId : userData.$id })
             console.log("transaction", transaction);
             if(transaction){
                 dispatch(addTransaction(transaction));
+                onSuccess();
             }
             
         } catch (error) {
@@ -34,7 +40,16 @@ function AddExpenseForm(){
 
     }
     return(
-        <form onSubmit={handleSubmit(submit)} className="w-80 md:w-120 mx-auto border-2 border-gray-200 shadow-sm rounded-2xl px-6 py-6">
+        <form onSubmit={handleSubmit(submit)} className="w-80 md:w-120 mx-auto bg-white border-2 border-gray-200 shadow-sm rounded-2xl px-6 py-6">
+            <div className="flex flex-row justify-between items-center mb-2">
+                <h1
+                 className="font-semibold text-2xl">
+                    Add Expense</h1>
+                <Button 
+                className="bg-white hover:bg-gray-50 w-fit p-2 rounded-xs" children="✖️"
+                onClick={onSuccess}
+                />
+            </div>
             <Input label="Name" placeholder="e.g. Grocery run" {...register("description", {required: true})}  />
             <Select label="Category" placeholder="Select a category..."
             options={["Grocery" , "Transportation" , "Foods" , "Shopping" , "Utilities" , "Entertainment" , "Health"]} 
