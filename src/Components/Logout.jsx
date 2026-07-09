00591146691957420f5486190414d02e5f99461a
Arif <arifmarahman19@gmail.com>
2026-07-09 21:37:31 +0100

    Modified for better control

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,22 +1,30 @@
 import { useDispatch } from "react-redux"
 import authService from "../appwrite/auth";
 import { logout } from "../slices/authSlice";
-import Button from "./Button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Logout(){
+    const [error , setError] = useState("");
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleLogout = async () => {
+        setError("");
         try {
             await authService.logout();
             dispatch(logout());
+            navigate("/login")
+            
             
         } catch (error) {
-            console.log(error)
+            console.log(error);
             
         }
     }
     return(
-        <Button onClick={handleLogout} children="Log out" className="bg-red-500 w-auto px-5" /> 
+        <button className="bg-red-500 text-white px-4 py-1 rounded-xl active:scale-98 hover:scale-105 cursor-pointer font-semibold duration-200 transition-all" onClick={handleLogout}>
+            Log out
+        </button> 
     )
 }
 export default Logout
